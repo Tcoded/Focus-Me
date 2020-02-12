8a9829a85105fd648bcb5a3d64b0be4c9083cd86
Joshua Trevena <trevenacoded@gmail.com>
2020-02-11 21:27:36 -0800

    implemented file writing function

--- mainApp.py
+++ mainApp.py
@@ -1,7 +1,8 @@
 import time
 from datetime import datetime as dtm
 
-hosts = r"C:\Windows\System32\drivers\etc"
+# hosts = r"C:\Windows\System32\drivers\etc"
+hosts = "hosts_temp"
 redirect = "127.0.0.1"
 blockList = ["www.facebook.com", "www.youtube.com", "www.twitter.com"]
 
@@ -10,6 +11,13 @@ while True:
         < dtm.now()
         < dtm(dtm.now().year, dtm.now().month, dtm.now().day, 17)):
         print("Get to work!")
+        with open(hosts, 'r+') as file:
+            hostsContent = file.read()
+            for site in blockList:
+                if site in hostsContent:
+                    pass
+                else:
+                    file.write("#" + " " + redirect + " " + site + '\n')
     else:
         print("Get off work!")
     time.sleep(10)
